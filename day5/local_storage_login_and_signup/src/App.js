e25d632fb94f831a756b806714d4f982fed5a629
Jackxzyte <55942658+Jackxzyte@users.noreply.github.com>
2023-06-12 17:50:05 +0530

    error handling

--- day5/local_storage_login_and_signup/src/App.js
+++ day5/local_storage_login_and_signup/src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye , faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import Home from './Home';
 import { useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function App() {
   
+  const [ showPassword , setShowPassword ] = useState(false);
+
+  const [ usernameExistsError , setUsernameExistsError ] = useState(false);
+  const [ passwordLengthError , setPasswordLengthError ] = useState(false);
   const [ usernameError ,setUsernameError ] = useState(false);
   const [ passwordError, setpasswordError ] = useState(false);
+  
   const [userName , setUserName] = useState('');
   const [signedUp , setSignedUp] = useState('');
   const [loggedIn , setLoggedIn] = useState(false);
+  
   const container = useRef();
+  
   const { register : signUpRegister , handleSubmit : signUpHandleSubmit } = useForm();
   const { register : loginRegister, handleSubmit : loginHandleSubmit } = useForm();
 
@@ -21,6 +30,17 @@ export default function App() {
       email: data.email,
       password: data.password
     };
+    
+    if(data.password.length < 8 ){
+      console.log('password length error');
+      setPasswordLengthError(true);
+      return;
+    }
+
+    if( localStorage.getItem('creds_'+data.username) ){
+      setUsernameExistsError(true);
+      return;
+    }
 
     localStorage.setItem('creds_'+data.username, JSON.stringify(creds));
     setSignedUp('Account Created! Login?');
@@ -53,17 +73,21 @@ export default function App() {
     }
   }
 
+  const toggleEye = () => {
+      setShowPassword(!showPassword);
+  }
+
   return (
     <>{ loggedIn ? <Home username={userName} setLoggedIn={setLoggedIn}/> :
         <div className="container" id="container" ref={container}>
           <div className="form-container sign-up-container">
-            
             <form action="#" onSubmit={signUpHandleSubmit(signUpClick)}>
               <h1>Create Account</h1><br/>
-              <input type="text" placeholder="Username" {...signUpRegister('username')} style={ {border: 'white'} }required/>
+              <input type="text" placeholder="Username" {...signUpRegister('username')} style={ usernameExistsError?{borderColor:'red'}:{border:'white'} } required/>
               <input type="email" placeholder="Email" {...signUpRegister('email')} style={ {border: 'white'} } required/>
-              <input type="password" placeholder="Password" {...signUpRegister('password')} style={ {border: 'white'} } required/>
-              <span>{signedUp}</span>
+              <input type={showPassword?"text":"password"} placeholder="Password" {...signUpRegister('password')} style={ passwordLengthError?{borderColor:'red'}:{border:'white'} } required/>
+              <FontAwesomeIcon className="password-toggle-signup" icon={!showPassword ? faEyeSlash : faEye} onClick={toggleEye}/>
+              <div>{signedUp}</div>
               <button>Sign Up</button>
             </form>
           
@@ -73,7 +97,8 @@ export default function App() {
             <form action="#" onSubmit={loginHandleSubmit(logInClick)} >
               <h1>Log in</h1><br/>
               <input type="text" placeholder="Username" {...loginRegister('username')} style={ usernameError?{borderColor:'red'}:{border:'white'} } required/>
-              <input type="password" placeholder="Password" {...loginRegister('password')} style={ passwordError?{borderColor:'red'}:{border:'white'} } required/>
+              <input type={showPassword?"text":"password"} placeholder="Password" {...loginRegister('password')} style={ passwordError?{borderColor:'red'}:{border:'white'} } required/>
+              <FontAwesomeIcon className="password-toggle" icon={!showPassword ? faEyeSlash : faEye} onClick={toggleEye}/>
               <button>Log In</button>
             </form>
 
@@ -84,6 +109,8 @@ export default function App() {
                 <h1>Welcome Back!</h1>
                 <p>Already have an Account?</p>
                 <button className="ghost" id="signIn" onClick={() => {
+                  setUsernameExistsError(false);
+                  setPasswordLengthError(false);
                   container.current.classList.remove("right-panel-active");}
                 } >Log In</button>
               </div>
@@ -91,6 +118,8 @@ export default function App() {
                 <h1>Welcome!</h1>
                 <p>Don't have an Account?</p>
                 <button className="ghost" id="signUp" onClick={() => {
+                  setUsernameError(false);
+                  setpasswordError(false);
                   container.current.classList.add("right-panel-active");}
                 }>Sign Up</button>
               </div>
