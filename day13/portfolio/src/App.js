8a01778771887193dccaed72ec787c9bf077e500
Jackxzyte <55942658+Jackxzyte@users.noreply.github.com>
2023-06-22 12:45:23 +0530

    day113 commit

---
+++ day13/portfolio/src/App.js
@@ -0,0 +1,22 @@
+
+export default function App() {
+  return (
+    <div className="outer-wrapper">
+      <div className='wrapper'>
+        
+        <div className='page one'>
+          <h1>Page 1</h1>
+        </div>
+
+        <div className='page two'>
+        <h1>Page 2</h1>
+        </div>
+
+        <div className='page three'>
+        <h1>Page 3</h1>
+        </div>
+     
+      </div>
+    </div>
+  );
+}
