fbc7ec42992b466fd56845c4ff92c86f4d2883ed
Jackxzyte <55942658+Jackxzyte@users.noreply.github.com>
2023-06-22 14:29:23 +0530

    day13

--- day13/portfolio/src/App.js
+++ day13/portfolio/src/App.js
@@ -1,21 +1,45 @@
+import './App.css';
+import { Parallax , ParallaxLayer } from '@react-spring/parallax';
+
 export default function App() {
   return (
-    <div className="outer-wrapper">
-      <div className='wrapper'>
-        
-        <div className='page one'>
-          <h1>Page 1</h1>
-        </div>
+    <div className="App">
+      <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
 
-        <div className='page two'>
-        <h1>Page 2</h1>
-        </div>
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div class="animation_layer parallax" id="hill1"></div>
+        </ParallaxLayer>
 
-        <div className='page three'>
-        <h1>Page 3</h1>
-        </div>
-     
-      </div>
+        <ParallaxLayer offset={0} speed={-0.1}>
+          <div class="animation_layer parallax" id="hill2"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="hill3"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.2}>
+          <div class="animation_layer parallax" id="hill4"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div class="animation_layer parallax" id="hill5"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.2}>
+          <div class="animation_layer parallax" id="tree"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={-0.5}>
+          <div class="animation_layer parallax" id="plant"></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={-0.2}>
+          <div class="animation_layer parallax" id="leaf"></div>
+        </ParallaxLayer>
+
+      </Parallax>
     </div>
   );
 }
+ 
