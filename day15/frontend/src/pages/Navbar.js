0243dd4ee0413593de7f364a8ba3363ffb2a0860
Jackxzyte <55942658+Jackxzyte@users.noreply.github.com>
2023-06-26 17:18:00 +0530

    frontend

---
+++ day15/frontend/src/pages/Navbar.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <header >
+        <div className='container'>
+            <Link to='/' ><h1>Task Manager</h1></Link>
+        </div>
+    </header>
+  )
+}
